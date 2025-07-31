e2225a17009dc59c3b8306afe43136e13162c398
Tú Đinh <trongtudinh@icloud.com>
2025-07-31 11:21:56 +0700

    Adds support for channel topics
    Adds support for channel topics by introducing new properties to channel payloads and DTOs.
    
    This includes:
    
    - Adding `topicsEnabled` and `isClosedTopic` properties to `ChannelCodingKeys`, `ChannelDetailPayload`, and `ChannelDTO`
    - Adding a `topics` property to `ChannelPayload` and `ChannelDTO`
    - Updating the database model to include these new attributes.
    
    These changes enable the application to handle and display channel topics.

--- Sources/ErmisChat/APIClient/Payloads/ChannelCodingKeys.swift
+++ Sources/ErmisChat/APIClient/Payloads/ChannelCodingKeys.swift
@@ -62,6 +62,4 @@ public enum ChannelCodingKeys: String, CodingKey, CaseIterable {
     case topicsEnabled = "topics_enabled"
     /// Close a topic flag.
     case isClosedTopic = "is_closed_topic"
-    ///
-    case invitedAt = "invited_at"
 }
